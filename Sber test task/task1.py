bac7653df60ab12da0da20d1d5f956a0cf29851b
omichan <nimfesta@mail.ru>
2022-08-10 09:52:27 +0600

    Update task1.py

--- Sber test task/task1.py
+++ Sber test task/task1.py
@@ -51,13 +51,90 @@ LIBS_WITH_CYCLES = [
 ]
 
 
+# recursive function for deep search depedencies
+def cross_dep(libs: List[Library], index: int, deped_stack: []):
+    # input: List of libraries, curent library index, list for all depedencies
+
+    # go by all depedencies
+    for item in libs[index].dependencies:
+        # check if depedency already added to list
+        if item in deped_stack:
+            break
+
+        # add current depedency to depedancy stack
+        deped_stack.append(item)
+
+        # rearch index of depedency in all libraries list
+        deped_index = [d for d in libs if d.name == item][0]
+
+        # add new depedencies to depedancies stack
+        deped_stack += cross_dep(libs, libs.index(deped_index), deped_stack)
+    # return all depedencies to load function
+    return deped_stack
+
+
 def load(libs: List[Library]):
     """
     Функция, которая выводит в консоль имена библиотек в порядке их загрузки.
 
     РЕАЛИЗУЙ ЭТУ ФУНКЦИЮ
     """
-    raise NotImplementedError
+    # Lenght of libraries list
+    libs_count = len(libs)
+
+    # list of loaded libs by func
+    libs_loaded = []
+
+    # Test for cross depedencies any libraries 
+    for i in range(libs_count):
+        
+        # List of all chain items
+        all_depedences_stack = []
+
+        # recursive func for fulling al
+        cross_dep(libs, i, all_depedences_stack)
+
+        #check all libraries chain for cross depedencies
+        if libs[i].name in all_depedences_stack:
+            print("Обнаружены перекрестные ссылки с библиотекой ", libs[i].name)
+            raise NotImplementedError
+
+    # get list of all libs
+    libs_stack = [item.name for item in libs]
+
+    # loading process flag
+    all_libs_loaded = False
+
+    # for loading control
+    last_loaded_count =0
+
+    # libs loading process 
+    while(not all_libs_loaded):
+        # go by lib index
+        for i in range(libs_count):
+            # skip loop if current lib loaded already
+            if libs[i].name in libs_loaded:
+                break
+
+            # check if can load library
+            if set(libs[i].dependencies).issubset(libs_loaded) or len(libs[i].dependencies)==0:
+                print("Загружена библиотека:", libs[i].name)
+                libs_loaded.append(libs[i].name)
+
+            # check if no independed library
+            if i >= libs_count-1 and len(libs_loaded)<=0:
+                print("В списке нет независимых пакетов")
+                raise NotImplementedError
+            
+            # if all libs are loaded  can end loop   
+            if set(libs_stack).issubset(libs_loaded):
+                all_libs_loaded = True
+
+        # check if depedencies from unexisting libraries    
+        if (len(libs_loaded) <= last_loaded_count):
+            print("Нет библиотек для загрузки")        
+            raise NotImplementedError
+        last_loaded_count = len(libs_loaded)
 
 
 
